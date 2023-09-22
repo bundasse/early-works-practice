26aae4f15b5c0ad2e39fcadad71f5fa7944c2ce9
bundasse <ymtotolove@gmail.com>
2023-09-22 21:49:47 +0900

    0922.Gnb 제작 시작

---
+++ src/components/GnbComp.vue
@@ -0,0 +1,20 @@
+<template lang="">
+    <div>
+        <ul>
+            <li><router-link to=""></router-link></li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    name: 'GnbComp',
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+<style lang="">
+    
+</style>
