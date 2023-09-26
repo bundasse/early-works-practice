46ea5a8796c30e51d71eb7c6a6f158e3a3cab6b4
bundasse <ymtotolove@gmail.com>
2023-09-26 15:30:35 +0900

    0926

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,10 +1,19 @@
 <template>
     <div class="px-10 py-10 flex justify-between flex-wrap bg-gray-100 gap-y-5">
         <h2 class="basis-full">메인화면</h2>
+        <!-- 파이 차트 -->
         <div v-for="(e, i) in PieData" :key="i" class="basis-[32.5%] py-5 rounded-2xl bg-white">
             <PieChart :chartData="e" />
         </div>
-        <div></div>
+        <!-- 프로그레스 서클 -->
+        <div class="basis-[22.5%] py-5 rounded-2xl bg-white">
+
+        </div>
+        <!--  -->
+        <div class="basis-[25%] px-5 py-5 rounded-2xl bg-white">
+
+        </div>
+        <!-- 라인 차트 -->
         <div class="basis-[65%] px-5 py-5 rounded-2xl bg-white">
             <LineChart :chartData="LineData"/>
         </div>
