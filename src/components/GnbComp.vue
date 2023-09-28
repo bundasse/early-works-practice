3d05d5cb5155ab0f9fffe6ae980d15da33bf0f12
bundasse <ymtotolove@gmail.com>
2023-09-28 22:56:41 +0900

    0928 알림 아이콘

--- src/components/GnbComp.vue
+++ src/components/GnbComp.vue
@@ -2,8 +2,9 @@
     <div class="col-span-full flex justify-between px-3 py-3 bg-cyan-600 text-white">
         <h1><router-link to="/">기업로고</router-link></h1>
         <ul class="flex gap-x-3 items-stretch">
-            <li>검색</li>
-            <li v-for="(e, i) in gnb" :key="i"><router-link to="">{{e.name}}</router-link></li>
+            <li class="relative after:absolute after:w-3 after:h-3 after:-right-1 after:-top-1 after:bg-red-600 after:rounded-full">검색</li>
+            <li class="relative after:absolute after:w-3 after:h-3 after:-right-1 after:-top-1 after:bg-red-600 after:rounded-full">알림</li>
+            <li class="relative after:absolute after:w-3 after:h-3 after:-right-1 after:-top-1 after:bg-red-600 after:rounded-full">메세지</li>
             <li>아이콘 프로필</li>
         </ul>
     </div>
@@ -13,7 +14,6 @@ export default {
     name: 'GnbComp',
     data() {
         return {
-            gnb: [{name:'알림'},{name:'메세지'}]
         }
     },
 }
